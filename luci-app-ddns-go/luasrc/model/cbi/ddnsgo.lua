d9cd2cbefea749fe3c4965adad61db4d46c3287e
zxlhhyccc <45259624+zxlhhyccc@users.noreply.github.com>
2022-10-22 20:58:00 +0800

    luci：Fixed an issue that required saving the application twice to enable and stop the application in the master branch

--- luci-app-ddns-go/luasrc/model/cbi/ddnsgo.lua
+++ luci-app-ddns-go/luasrc/model/cbi/ddnsgo.lua
@@ -2,25 +2,26 @@
 
 local m, s ,o
 
-local m = Map("ddnsgo", translate("DDNS-GO"), translate("DDNS-GO automatically obtains your public IPv4 or IPv6 address and resolves it to the corresponding domain name service.")..translate("</br>For specific usage, see:")..translate("<a href=\'https://github.com/sirpdboy/luci-app-ddns-go.git' target=\'_blank\'>GitHub @sirpdboy/luci-app-ddns-go </a>") )
+m = Map("ddnsgo")
+m.title = translate("DDNS-GO")
+m.description = translate("DDNS-GO automatically obtains your public IPv4 or IPv6 address and resolves it to the corresponding domain name service.")..translate("</br>For specific usage, see:")..translate("<a href=\'https://github.com/sirpdboy/luci-app-ddns-go.git' target=\'_blank\'>GitHub @sirpdboy/luci-app-ddns-go </a>")
 
-m:section(SimpleSection).template  = "ddnsgo_status"
+m:section(SimpleSection).template = "ddnsgo_status"
 
 s = m:section(TypedSection, "ddnsgo", translate("Global Settings"))
-s.addremove=false
-s.anonymous=true
+s.addremove = false
+s.anonymous = true
 
-o=s:option(Flag,"enabled",translate("Enable"))
-o.default=0
+o = s:option(Flag,"enabled",translate("Enable"))
+o.default = 0
 
-o=s:option(Value, "port",translate("Set the DDNS-TO access port"))
-o.datatype="uinteger"
+o = s:option(Value, "port",translate("Set the DDNS-TO access port"))
+o.datatype = "uinteger"
 o.default=9876
 
-
-local e=luci.http.formvalue("cbi.apply")
-if e then
-  io.popen("/etc/init.d/ddnsgo start")
+m.apply_on_parse = true
+m.on_after_apply = function(self,map)
+	luci.sys.exec("/etc/init.d/ddnsgo restart")
 end
 
 return m
